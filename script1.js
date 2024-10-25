3bc33d466de0c46754e82d5575eb3b423b75a9b5
Võ Tấn Sang <22130233@st.hcmuaf.edu.vn>
2024-10-25 23:00:23 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+// Tab switching functionality
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Automatically display the Overview tab by default
+document.querySelector('.tablinks').click();
